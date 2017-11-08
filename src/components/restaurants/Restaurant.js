f96afe8174421133e8b0cd5eda1e23250250012a
Allen Anderson <dande313@gmail.com>
2017-11-08 13:48:11 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id,
+    })
+  }
   render() {
     const {text, id } = this.props.restaurant;
     return (
