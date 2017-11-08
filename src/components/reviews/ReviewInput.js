63f8543d3f9c2a6f280381ec2955c2165bc236f1
Allen Anderson <dande313@gmail.com>
2017-11-08 13:37:00 +0000

    automatically backed up by learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: {
+        text: this.state.text,
+        restaurantId: this.props.restaurantId,
+      },
+    });
+    this.setState({
+      text: '',
+    });
+  }
   render() {
     return (
       <div>
